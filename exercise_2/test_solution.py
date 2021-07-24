92aeaeed4eff0bb3fe306c68735e5af64bb4616f
JEFF HOLLAND <jeff@JEFFs-MacBook-Pro.local>
2021-07-24 16:03:48 -0500

    Updated pytest code and test results .png file

--- exercise_2/test_solution.py
+++ exercise_2/test_solution.py
@@ -1,35 +1,22 @@
 from solution import Item, Cart
 
-
-def test_item():
-    item = Item(2, 'grain', 'rice', 1)
-    assert item.quantity == 2
-    assert item.measure == 'grain'
-    assert item.name == 'rice'
-    assert item.price == 1
-    assert str(item) == '    2 grain rice       @ $1.0...$2.0'
-
-
 def test_empty_cart():
     cart = Cart()
-    assert cart.items == []
+    cart.cart_list == []
     assert f'{cart:short}' == ''
     assert f'{cart:long}' == ''
 
-
 def test_cart_1_item():
     cart = Cart()
-    assert len(cart.items) == 0
+    assert len(cart.cart_list) == 0
 
     item = Item(2, 'grain', 'rice', 1)
     cart.add(item)
-    assert len(cart.items) == 1
-    assert cart.items[0] == item
+    assert len(cart.cart_list) == 1
+    assert cart.cart_list[0] == item
 
     assert f'{cart:short}' == 'rice'
-    assert f'{cart:long}' == f'\t{item}'
-    assert f'{cart:garbage}' == 'unknown format code garbage'
-
+    assert f'{cart:long}' == '    2 grain     rice @ $1.00...$2.00\n'
 
 def test_cart_2_item2():
     cart = Cart()
@@ -38,9 +25,9 @@ def test_cart_2_item2():
     cart.add(rice)
     cart.add(bread)
 
-    assert len(cart.items) == 2
-    assert cart.items[0] == rice
-    assert cart.items[1] == bread
+    assert len(cart.cart_list) == 2
+    assert cart.cart_list[0] == rice
+    assert cart.cart_list[1] == bread
 
-    assert f'{cart:short}' == 'bread, rice'
-    assert f'{cart:long}' == f'\t{bread}\n\t{rice}'
+    assert f'{cart:short}' == 'rice, bread'
+    assert f'{cart:long}' == '    2 grain     rice @ $1.00...$2.00\n    1 loaf      bread @ $1.00...$1.00\n'
