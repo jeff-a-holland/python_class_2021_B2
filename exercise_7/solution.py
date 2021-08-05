f8f17d861d4a4be332d6a4ec1ebce488e15d878f
JEFF HOLLAND <jeff@JEFFs-MacBook-Pro.local>
2021-08-05 08:20:36 -0500

    Updates

--- exercise_7/solution.py
+++ exercise_7/solution.py
@@ -1,1 +1,36 @@
 #!/Users/jeff/.pyenv/shims/python
+import time
+
+class TooSoonError(Exception):
+    """Custom Exception"""
+    def __init__(self, value):
+        message = f'Wait another {value} seconds'
+        super().__init__(message)
+
+start_time = time.time()
+def once_per_minute(func):
+    """Decorator function that only allows a function passed as an arg to run
+    once a minute."""
+    def wrapper(*args):
+        global start_time
+        delta = 60 - (time.time() - start_time)
+
+        if i == 0:
+            return func(args[0])
+        elif i > 0 and delta > 0:
+            raise TooSoonError(delta)
+        else:
+            start_time = time.time()
+            return func(args[0])
+    return wrapper
+
+@once_per_minute
+def hello(name):
+    return "Hello, {}".format(name)
+
+for i in range(30):
+    try:
+        time.sleep(3)
+        print(hello("attempt {}".format(i)))
+    except TooSoonError as e:
+        print("Too soon: {}".format(e))
--- exercise_7/test_solution.py
+++ exercise_7/test_solution.py
@@ -1,1 +1,34 @@
 #!/Users/jeff/.pyenv/shims/python
+
+import pytest
+import time
+from solution import once_per_minute, TooSoonError
+
+@once_per_minute
+def hello(name):
+    return f"Hello, {name}"
+
+def test_run_once(capsys):
+    time.sleep(60)
+    assert hello('world') == 'Hello, world'
+
+def test_run_twice_in_a_row(capsys):
+    with capsys.disabled():
+        print("Waiting to run")
+    time.sleep(60)
+    assert hello('world') == 'Hello, world'
+
+    with pytest.raises(TooSoonError) as e:
+        hello('world 2')
+
+def test_run_waiting_59_sec(capsys):
+    with capsys.disabled():
+        print("Waiting to run")
+    time.sleep(60)
+
+    assert hello('world') == 'Hello, world'
+    time.sleep(59)
+
+    with pytest.raises(TooSoonError) as e:
+        hello('world 2')
+        assert 'Wait another 1.' in str(e)
