0773de4be3cea5344383752feb2cd17645eb5f6c
JEFF HOLLAND <jeff@JEFFs-MacBook-Pro.local>
2021-08-05 10:05:27 -0500

    Updates

--- exercise_7/solution.py
+++ exercise_7/solution.py
@@ -7,7 +7,9 @@ class TooSoonError(Exception):
         message = f'Wait another {value} seconds'
         super().__init__(message)
 
+## Set start_time global var
 start_time = time.time()
+
 def once_per_minute(func):
     """Decorator function that only allows a function passed as an arg to run
     once a minute."""
@@ -15,13 +17,18 @@ def once_per_minute(func):
         global start_time
         delta = 60 - (time.time() - start_time)
 
+        ## Create arg_str to handle multiple args of different types given to
+        ## the decorated function.
+        arg_list = [str(value) for value in args]
+        arg_str = ' '.join(arg_list)
+
         if i == 0:
-            return func(args[0])
+            return func(arg_str)
         elif i > 0 and delta > 0:
             raise TooSoonError(delta)
         else:
             start_time = time.time()
-            return func(args[0])
+            return func(arg_str)
     return wrapper
 
 @once_per_minute
@@ -31,6 +38,9 @@ def hello(name):
 for i in range(30):
     try:
         time.sleep(3)
+        ## STDOUT that works with pytest code.
         print(hello("attempt {}".format(i)))
+        ## Test multiple args of different types. Will not work with pytest code.
+        #print(hello([1,2,3], 'jeff'))
     except TooSoonError as e:
         print("Too soon: {}".format(e))
