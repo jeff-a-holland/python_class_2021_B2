9b8c61ad4c0a0464004508ac9d119c0d006d27a7
JEFF HOLLAND <jeff@JEFFs-MacBook-Pro.local>
2021-08-17 14:30:48 -0500

    Updates

--- exercise_9/solution.py
+++ exercise_9/solution.py
@@ -1,1 +1,50 @@
 #!/Users/jeff/.pyenv/shims/python
+
+class Foo(object):
+    """Implement eq, hash, and str methods to make the Foo class return a unique
+    value only once when tyring to add to a set multiple times"""
+    def __init__(self, x):
+        self.x = x
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+    def __hash__(self):
+        return hash((self.x))
+
+    def __str__(self):
+        return f'{self.x}'
+
+class Uniquish(object):
+    """Implement eq, hash, and str methods to make the Uniquish class return a
+    unique value only once when tyring to add to a set multiple times"""
+    def __init__(self, x):
+        self.x = x
+
+    def __eq__(self, other):
+        return self.x == other.x
+
+    def __hash__(self):
+        return hash((self.x))
+
+    def __str__(self):
+        return f'{self.x}'
+
+## Test Foo class. Should return a set of length 1.
+f1 = Foo(10)
+f2 = Foo(10)
+f3 = Foo(10)
+s = {f1, f2, f3}
+print('\nFoo result:', s)
+
+## Test Uniquish class by creating Bar class and inherit Uniquish class. Should
+## return a set of length 1.
+class Bar(Uniquish):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+b1 = Bar(10, 'abc')
+b2 = Bar(10, 'abc')
+b3 = Bar(10, 'abc')
+s = {b1, b2, b3}
+print('\nUniquish result:', s, '\n')
--- exercise_9/test_solution.py
+++ exercise_9/test_solution.py
@@ -1,1 +1,43 @@
 #!/Users/jeff/.pyenv/shims/python
+from solution import Foo, Uniquish
+
+def test_foo():
+    f1 = Foo(10)
+    f2 = Foo(10)
+    f3 = Foo(10)
+
+    s = {f1, f2, f3}
+
+    assert len(s) == 1
+    assert hash(f1) == hash(f2)
+    assert hash(f2) == hash(f3)
+
+def test_subclass_non_uniquish():
+    class Bar():
+        def __init__(self, x):
+            self.x = x
+
+    b1 = Bar(10)
+    b2 = Bar(10)
+    b3 = Bar(10)
+
+    s = {b1, b2, b3}
+
+    assert len(s) == 3
+    assert hash(b1) != hash(b2)
+    assert hash(b2) != hash(b3)
+
+def test_subclass_uniquish():
+    class Bar(Uniquish):
+        def __init__(self, x):
+            self.x = x
+
+    b1 = Bar(10)
+    b2 = Bar(10)
+    b3 = Bar(10)
+
+    s = {b1, b2, b3}
+
+    assert len(s) == 1
+    assert hash(b1) == hash(b2)
+    assert hash(b2) == hash(b3)
